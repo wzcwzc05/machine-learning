62a2168e7b12cfd3c6e9bcf103a1536728d32cf6
wzcwzc05 <zichenw98@gmail.com>
2024-10-31 17:23:17 +0800

    Complete Matrix Class

--- cpp/include/matrix.hpp
+++ cpp/include/matrix.hpp
@@ -1,6 +1,7 @@
+#pragma once
 #include <cmath>
-#include <vector>
 #include <iostream>
+#include <vector>
 namespace mt {
 template <typename T>
 class MMatrix {
@@ -27,6 +28,12 @@ class MMatrix {
             }
         }
     }
+    MMatrix(int rows, int cols, T val) : rows_(rows), cols_(cols) {
+        this->data_ = new T[rows * cols];
+        for (int i = 0; i < rows * cols; i++) {
+            this->data_[i] = val;
+        }
+    }
     ~MMatrix() { delete[] data_; }
     T &operator()(int i,
                   int j) {  // 重载()运算符，获取元素值，i表示行，j表示列
@@ -35,28 +42,26 @@ class MMatrix {
         }
         return data_[i * cols_ + j];
     }
-    // Matrix &operator=(const Matrix &other);  // 拷贝赋值运算符
-    // Matrix operator+(const Matrix &a,
-    //                  const Matrix &b);  // 矩阵加法（元素相加）
-    // Matrix operator*(const Matrix &a,
-    //                  const Matrix &b);  // 矩阵乘法（元素相乘）
-    // Matrix operator-(const Matrix &a,
-    //                  const Matrix &b);  // 矩阵减法（元素相减）
-    // Matrix operator/(const Matrix &a,
-    //                  const Matrix &b);  // 矩阵除法（元素相除）
-    MMatrix cdot(const MMatrix &a);   // 矩阵点乘
-    MMatrix transpose();              // 矩阵转置
-    MMatrix inverse();                // 矩阵求逆
-    double determinant();             // 矩阵行列式
-    MMatrix submatrix(int i, int j);  // 子矩阵
-    MMatrix submatrix(int i, int j, int rows,
-                      int cols);  // 从第i行第j列提取rows行cols列的子矩阵
-    MMatrix minor(int i, int j);    // 矩阵的第i行第j列的余子式
-    double cofactor(int i, int j);  // 矩阵的第i行第j列的代数余子式
-    MMatrix adjoint();              // 矩阵的伴随矩阵
-    MMatrix trace();                // 矩阵的迹
-    void print();                   // 打印矩阵
-    void set(int i, int j, T val);  // 设置元素值
-    T get(int i, int j) const;      // 获取元素值
+    int rows() const;
+    int cols() const;
+    MMatrix<T> operator+(const MMatrix &a);  // 矩阵加法（元素相加）
+    MMatrix<T> operator*(const MMatrix &a);  // 矩阵乘法（元素相乘）
+    MMatrix<T> operator-(const MMatrix &a);  // 矩阵减法（元素相减）
+    MMatrix<T> operator/(const MMatrix &a);  // 矩阵除法（元素相除）
+    MMatrix<T> cdot(const MMatrix &a);       // 矩阵点乘
+    MMatrix<T> transpose();                  // 矩阵转置
+    MMatrix<T> inverse();                    // 矩阵求逆
+    double det();                         // 矩阵行列式
+    MMatrix<T> submatrix(int i, int j);      // 子矩阵
+    MMatrix<T> submatrix(int i, int j, int rows,
+                      int cols);     // 从第i行第j列提取rows行cols列的子矩阵
+    MMatrix<T> minor(int i, int j);     // 矩阵的第i行第j列的余子式
+    MMatrix<T> cofactor(int i, int j);  // 矩阵的第i行第j列的代数余子式
+    MMatrix<T> adjoint();               // 矩阵的伴随矩阵
+    T trace();                 // 矩阵的迹
+    void print();                    // 打印矩阵
+    void set(int i, int j, T val);   // 设置元素值
+    T get(int i, int j) const;       // 获取元素值
 };
 };  // namespace mt
+#include "matrix.cpp"
